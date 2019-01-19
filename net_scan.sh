96133ed027856e4c43ecc0261bff63946e5c34fb
Alberto Barea <alberto.bareabr@gmail.com>
2019-01-19 13:34:48 +0100

    Included analyse_ip inside net_scan flow

--- net_scan.sh
+++ net_scan.sh
@@ -11,8 +11,20 @@
 
 #MACS_PARSED=$(sudo nmap -sn 192.168.1.0/24 | awk '/MAC Address:/{ print $3 }' | sed ':a;N;$!ba;s/\n/|/g')
 
+# Runs an OS detection scan of the UNKOWN_IP array
+analyse_ip () {	
+	UNKNOWN_IP=$1	
+	for IP in $UNKNOWN_IP; do
+		echo "$IP:"
+		echo '---------------------------------'
+		sudo nmap -sV -O -v $IP
+		echo '---------------------------------'
+	done
+}
+
 MACS_FILE=$1
 WARNING_FILE=$2
+IP_FILE=$3
 
 NMAP_RESULT=$(nmap -sn 192.168.1.0/24)
 
@@ -34,11 +46,23 @@ echo '---------------------------------'
 # If the mac address is not in the MACS_FILE, then it's not recognized
 INDEX=0
 DATE=$(date '+%d/%m/%Y %T')
+UNKNOWN_IP=()	#Array which contains the IPs associated with the MACs we don't recognize
 
+# We compare if the MACs detected are inside the MACS_FILE
 for MAC in $RAW_MACS; do
     if ! grep -q $MAC $MACS_FILE
 	then
     	echo "[$DATE] $MAC - ${IPS_ARRAY[$INDEX]} is not recognized" | tee -a $WARNING_FILE	# We echo to standard output and to a file with tee
+		UNKNOWN_IP+=(${IPS_ARRAY[$INDEX]})
 	fi
 	((INDEX++))
 done
+
+# If UNKNOWN_IP is not empty, we analyse the IPs detected
+if ! [ -z "$UNKNOWN_IP" ]
+	then		
+		echo '---------------------------------'
+		echo "Analizing IPs detected..."
+    	analyse_ip "$UNKNOWN_IP" >> $IP_FILE	
+		echo "Done"
+fi
